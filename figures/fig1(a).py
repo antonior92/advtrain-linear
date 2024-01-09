32cf37390c077e0a18ad7039fce0c5551612e413
antonio <antonior92@gmail.com>
2024-01-09 14:48:38 +0100

    improv

--- figures/fig1(a).py
+++ figures/fig1(a).py
@@ -4,10 +4,28 @@ import sklearn.model_selection
 from sklearn.datasets import load_diabetes
 from sklearn.linear_model import ElasticNetCV, LassoLarsIC, Lasso
 from ucimlrepo import fetch_ucirepo, list_available_datasets
+import matplotlib as mpl
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-def nerror(X_test, y_test, param):
-    y_pred = X_test @ param
-    return np.sqrt((y_test - y_pred) ** 2 / np.mean(y_test ** 2))
+
+# Basic style
+plt.style.use(['mystyle.mpl'])
+
+# Additional style
+mpl.rcParams['figure.figsize'] = 7, 3
+mpl.rcParams['figure.subplot.bottom'] = 0.15
+mpl.rcParams['figure.subplot.right'] = 0.99
+mpl.rcParams['figure.subplot.top'] = 0.95
+mpl.rcParams['font.size'] = 22
+mpl.rcParams['legend.fontsize'] = 20
+mpl.rcParams['legend.handlelength'] = 1
+mpl.rcParams['legend.handletextpad'] = 0.01
+mpl.rcParams['xtick.major.pad'] = 7
+
+def compute_R2(y_pred, y_test):
+    y_mean = np.mean(y_test)
+    return 1 - np.mean((y_test - y_pred) ** 2) / np.mean((y_test - y_mean) ** 2)
 
 def normalize(X_train, X_test, y_train, y_test):
     X_mean = X_train.mean(axis=0)
@@ -46,7 +64,7 @@ def wine():
 def abalone():
     dset = fetch_ucirepo(name="Abalone")
     F = dset.data.features
-    F.loc['Sex'] = (F['Sex'] == 'M').values.astype(float)
+    F = F.assign(Sex=(F['Sex'] == 'M').values.astype(float))
     X = F.values
     y = dset.data.targets.values.flatten()
     X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.3)
@@ -94,35 +112,63 @@ if __name__ == '__main__':
 
     # Compute performance
     df = {}
-    for dset in [heart_failure,]:
-        df[dset.__name__] = pd.DataFrame({'dset': [], 'method': [], 'nerror': []})
+    for dset in []:
+        df[dset.__name__] = pd.DataFrame({'dset': [], 'method': [], 'y_pred': []})
         for method in [lasso_cv, advtrain]:
             X_train, X_test, y_train, y_test = dset()
             param = method(X_train, y_train)
             n_test = len(y_test)
             df1 = pd.DataFrame({'dset': n_test * [dset.__name__],
                                 'method': n_test * [method.__name__],
-                                'nerror': nerror(X_test, y_test, param)})
+                                'y_pred': X_test @ param,
+                                'y_test': y_test})
 
             df[dset.__name__] = pd.concat([df[dset.__name__], df1])
         df[dset.__name__].to_csv(f'fig1(a){dset.__name__}.csv')
 
-
     if load_data:
         df = {}
-        for dset in [magic, diabetes, wine, abalone, heart_failure]:
+        for dset in [diabetes, wine, abalone, heart_failure]:
             df[dset.__name__] = pd.read_csv(f'fig1(a){dset.__name__}.csv')
 
-    df_final = pd.concat(df.values())
+    df_final = pd.DataFrame({'dset': [], 'method': [], 'R2': []})
+    for dset in [heart_failure, diabetes, wine, abalone]:
+        for method in [lasso_cv, advtrain]:
+            df_aux = df[dset.__name__].loc[df[dset.__name__]['method'] == method.__name__]
 
-    import seaborn as sns
-    import matplotlib.pyplot as plt
-    plt.style.use('./mystyle.mpl')
+            value_r2 = compute_R2(df_aux['y_pred'], df_aux['y_test'])
+
+            n_boot = 500
+            value_r2_bootstrap = np.zeros(n_boot)
+            for i in range(n_boot):
+                indexes = np.random.choice(range(len(df_aux['y_pred'])), len(df_aux['y_pred']))
+                df_permuted = df_aux.iloc[indexes]
+                value_r2_bootstrap[i] = compute_R2(df_permuted['y_pred'], df_permuted['y_test'])
+
+            df_final = df_final.append({
+                'dset': dset.__name__,
+                'method': method.__name__,
+                'R2': value_r2}, ignore_index=True)
+
+            df_final = df_final.append({
+                'dset': dset.__name__,
+                'method': method.__name__,
+                'R2': np.quantile(value_r2_bootstrap, 0.025)}, ignore_index=True)
+
+            df_final = df_final.append({
+                'dset': dset.__name__,
+                'method': method.__name__,
+                'R2': np.quantile(value_r2_bootstrap, 0.975)}, ignore_index=True)
+
+
+    plt.figure()
     sns.color_palette("hls", 8)
-    sns.pointplot(data=df_final, x='dset', y='nerror', hue='method', dodge=.4, linestyles="none", errorbar=('pi', 0.5),
-                  markers="o", markersizes=20, markeredgewidths=3,  capsize=0.05, n_boot=500, seed=10)
+    sns.pointplot(data=df_final, x='dset', y='R2', hue='method', dodge=.4, estimator='median', linestyles="none",
+                  errorbar=('pi', 100), capsize=0.05)
     plt.xlabel('')
-    plt.ylabel('RMSE')
+    plt.ylabel('$$R^2$$')
+    plt.ylim((0, 1))
+    plt.legend(title='')
     plt.savefig('fig1(a).pdf')
     plt.show()
 
