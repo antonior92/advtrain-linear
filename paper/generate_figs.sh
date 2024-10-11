1e591daa92de5274d9bab80c387c76ae47e1ca4d
antonio <antonior92@gmail.com>
2024-10-11 11:34:39 +0200

    Uppdate figure order

--- paper/generate_figs.sh
+++ paper/generate_figs.sh
@@ -15,7 +15,7 @@ python 2-time-regression.py --load_data --dont_show
 
 
 # ----------------------
-# --- Figure 2 and 3 ---
+# --- Figure 2 + S.1 ---
 # ----------------------
 for DSET in mnist;
 do
@@ -35,19 +35,28 @@ python 3-convergence-gd.py --setting compare_lr  --dset breast_cancer  --n_iter 
 python 3-convergence-gd.py --setting acceleration --dset breast_cancer  --n_iter 100 --dont_show
 python 3-convergence-gd.py --setting stochastic --dset breast_cancer --n_iter 100 --xlabel "\#  epochs" --dont_show
 
+
+
+# -----------------------------
+# --- Fig 3(top) ---
+# -----------------------------
+# Top (Deterministic)
+python 3-convergence-gd.py --setting fgsm --dset breast_cancer --n_iter 1000 --dont_plot
+python 3-convergence-gd.py --setting fgsm --dset breast_cancer --n_iter 500 --load_data --dont_plot
+# Bottom (Stochastic)
+python 3-convergence-gd.py --setting fgsm-sgd --dset breast_cancer --n_iter 100 --dont_plot
+
+
 # --------------------------
 # --- Figure 4 top + S.2 ---
 # --------------------------
 python 4-convergence-regression.py --dset abalone
-
-
 python 4-convergence-regression.py --dset wine --n_iter 30
 python 4-convergence-regression.py --dset diabetes --n_iter 100
 
 # -----------------------
 # --- Figure 4 bottom ---
 # -----------------------
-# now running on Hyperion (screen  253869.time-eval)
 python 5-syntetic-dsets.py --setting comparing_advtrain_linf_methods --dont_plot --n_points 30 --increasing_scale 1000
 python 5-syntetic-dsets.py --setting comparing_advtrain_linf_methods --plot_type time --load_data
 
@@ -58,63 +67,38 @@ for SETTING in spiked_covariance sparse_gaussian;
   do python 5-syntetic-dsets.py  --setting $SETTING  --dont_show
 done;
 
-# ------------------
-# --- Figure S.1 ---
-# ------------------
-
-
-# ------------------
-# --- Figure S.2 ---
-# ------------------
-python 1-performance.py --setting classif
-
-
-# ------------------
-# --- Figure S.3 ---
-# ------------------
-# now running on Hyperion (screen  253869.time-eval)
-python 5-syntetic-dsets.py --setting comparing_advtrain_linf_methods_classif --dont_plot --n_points 10 --increasing_scale 50
-python 5-syntetic-dsets.py --setting comparing_advtrain_linf_methods_classif --plot_type time --load_data
-
 
 
 # -----------------------------
-# --- Neurips Rebutal Fig 1(a) ---
-# -----------------------------
-python 3-convergence-gd.py --setting fgsm --dset breast_cancer --n_iter 1000 --dont_plot
-python 3-convergence-gd.py --setting fgsm --dset breast_cancer --n_iter 500 --load_data --dont_plot
-
-
-# -----------------------------
-# --- Neurips Rebutal Fig 1(b) ---
-# -----------------------------
-python 3-convergence-gd.py --setting fgsm-sgd --dset breast_cancer --n_iter 100 --dont_plot
-
-
-# -----------------------------
-# --- Neurips Rebutal Fig 2 ---
-# -----------------------------
-python 3-convergence-gd.py --setting fgsm-sgd --dset breast_cancer --n_iter 100 --dont_plot
-
-
-
-# -----------------------------
-# --- Neurips Rebutal Fig 2 ---
+# --- Fig S.3 ---
 # -----------------------------
 python 3-convergence-gd.py --setting batch_size --dset breast_cancer --n_iter 1000 --dont_plot
 python 3-convergence-gd.py --setting batch_size --dset breast_cancer --n_iter 500 --load_data --dont_plot
 
 
+
+# ------------------
+# --- Figure S.5 ---
+# ------------------=
+python 5-syntetic-dsets.py --setting comparing_advtrain_linf_methods_classif --dont_plot --n_points 10 --increasing_scale 50
+python 5-syntetic-dsets.py --setting comparing_advtrain_linf_methods_classif --plot_type time --load_data
+
+# ------------------
+# --- Figure S.6 ---
+# ------------------
+python 1-performance.py --setting classif
+
+
+
 # -----------------------------
-# --- Neurips Rebutal Fig 3 ---
+# --- Table S.1---
 # -----------------------------
-# running 742260.magic
+python comparing_cvxpy_configs.py
+
+
+# -----------------------------
+# --- Extra ---
+# -----------------------------
 python 5-syntetic-dsets.py --setting magic --n_reps 5 --dont_plot
 python 5-syntetic-dsets.py --setting magic --n_reps 5 --load_data
 
-
-# -----------------------------
-# --- Neurips Rebutal Table CVXPY ---
-# -----------------------------
-# running hyperion:cvxpy
-python comparing_cvxpy_configs.py
